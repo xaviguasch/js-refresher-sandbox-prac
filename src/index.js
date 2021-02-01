42503c1d89304da8dd201ffe75b40407c2773060
xaviguasch <xaviguasch@gmail.com>
2021-02-01 17:58:01 +0100

    added solution for fizz buzz challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hey hey hey')
+// Fizz Buzz
+
+for (let i = 0; i < 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i, 'FizzBuzz')
+  } else if (i % 3 === 0) {
+    console.log(i, 'Fizz')
+  } else if (i % 5 === 0) {
+    console.log(i, 'Buzz')
+  } else {
+    console.log(i)
+  }
+}

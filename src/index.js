d6ed2171e823b14b425385b70fe95f4b37dc3668
xaviguasch <xaviguasch@gmail.com>
2021-02-01 21:13:09 +0100

    solved previous exercise

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 // Fizz Buzz
 
-for (let i = 1; i < 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i, 'FizzBuzz')
-  } else if (i % 3 === 0) {
-    console.log(i, 'Fizz')
-  } else if (i % 5 === 0) {
-    console.log(i, 'Buzz')
-  } else {
-    console.log(i)
-  }
+// for (let i = 1; i < 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i, 'FizzBuzz')
+//   } else if (i % 3 === 0) {
+//     console.log(i, 'Fizz')
+//   } else if (i % 5 === 0) {
+//     console.log(i, 'Buzz')
+//   } else {
+//     console.log(i)
+//   }
+// }
+
+// Functions exercise
+
+const bob = document.querySelector('.face')
+let count = 0
+
+function moveBob() {
+  count += 50
+  bob.style.left = count + 'px'
 }
+
+bob.addEventListener('click', moveBob)
